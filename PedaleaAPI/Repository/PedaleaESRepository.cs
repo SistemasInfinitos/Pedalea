cc99ee68144adfb26761cf0df9183dfce95ddf8b
MAURICIO BELLO <bello900@hotmail.com>
2022-07-25 23:36:30 -0500

    GetPersonas

--- PedaleaAPI/Repository/PedaleaESRepository.cs
+++ PedaleaAPI/Repository/PedaleaESRepository.cs
@@ -48,6 +48,7 @@ namespace PedaleaAPI.Repository
                     };
                     personas.Add(employee);
                 }
+                con.Close();
                 return (personas);
             }
         }
