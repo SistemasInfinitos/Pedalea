25df23aea8238a8511428ed6b9c288b28debcc71
MAURICIO BELLO <bello900@hotmail.com>
2022-07-25 20:15:47 -0500

    Add project files.

---
+++ PedaleaAPI/Controllers/PersonasController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PedaleaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonasController : ControllerBase
+    {
+    }
+}
