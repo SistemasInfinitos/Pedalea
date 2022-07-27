60175fc090af4ae58ca5518377a57e1c6d379807
MAURICIO BELLO <bello900@hotmail.com>
2022-07-27 12:13:10 -0500

    GetProductoByName

--- PedaleaWEB/Controllers/Pedalea/PedaleaAPIController.cs
+++ PedaleaWEB/Controllers/Pedalea/PedaleaAPIController.cs
@@ -136,6 +136,73 @@ namespace PedaleaWEB.Controllers.Pedalea
 
             responseClient.Errors.Add("Su sesión a Caducado, Acceso denegado!");
             return BadRequest(new { model, responseClient });
+        }       
+        
+        [Route("[action]", Name = "GetProductoByName")]
+        [HttpGet]
+        public async Task<IActionResult> GetProductoByName(string name)
+        {
+            var options = new JsonSerializerOptions { IncludeFields = true, PropertyNameCaseInsensitive = true };
+            await HttpContext.Session.LoadAsync();
+            HttpContext.Session.SetString("token", "el token desde el login");
+
+            string? accessToken = HttpContext.Session.GetString("token");
+            AuthResult responseClient = new AuthResult();
+            var model = new List<Productos>();
+            if (!string.IsNullOrWhiteSpace(accessToken))
+            {
+                var httpClient = new HttpClient();
+                httpClient.DefaultRequestHeaders.Add("Authorization", "Bearer " + accessToken);
+
+                if (ModelState.IsValid)
+                {
+                    string uri = $"https://localhost:7107/api/Pedalea/GetProductoByName=name={name}"; // esto se debe configurar en el archivo json
+
+                    try
+                    {
+                        var response = await httpClient.GetAsync(uri);
+
+                        if (response.IsSuccessStatusCode)
+                        {
+                            model = await JsonSerializer.DeserializeAsync<List<Productos>>(await response.Content.ReadAsStreamAsync(), options);
+
+                            if (model!=null && model.Count > 0)
+                            {
+                                responseClient.Success = true;
+                                responseClient.Mensaje = "success";
+                                return Ok(new { model, responseClient });
+                            }
+                            else if (model == null || model.Count == 0)
+                            {
+                                responseClient.Success = true;
+                                responseClient.Mensaje = "Sin datos";
+                                return Ok(new { model, responseClient });
+                            }
+                            else
+                            {
+                                responseClient.Errors.Add("Acceso denegado!");
+                                return BadRequest(new { model, responseClient });
+                            }
+                        }
+                        else
+                        {
+                            responseClient.Errors = new List<string>() { "Acceso denegado!" };
+                            responseClient.Errors.Add(response.EnsureSuccessStatusCode().StatusCode.ToString());
+
+                            return BadRequest(new { model, responseClient });
+                        }
+                    }
+                    catch (Exception x)
+                    {
+                        responseClient.Errors.Add(x.Message);
+                        responseClient.Errors.Add(x.InnerException?.Message ?? "Exception!");
+                        return BadRequest(new { model, responseClient });
+                    }
+                }
+            }
+
+            responseClient.Errors.Add("Su sesión a Caducado, Acceso denegado!");
+            return BadRequest(new { model, responseClient });
         }
 
         [HttpDelete]
